c0428df0ebd4e283025ec5c78987685e8ecf2b6d
Nick Bilsborrow <nickbilsborrow15@gmail.com>
2021-07-05 11:51:59 +0100

    error handling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import {useState} from "react"
 
 const App = () => {
   const [data,setdata]= useState("");
+  const [error,seterror]= useState({error:false, message:""})
 
   const handleFetch = async() =>{
+    try{
     const response = await fetch( "https://tronalddump.io/random/quote");
+    if(response.status !== 200){
+      throw new error("oops!");
+    }
     const data = await response.json();
     setdata(data);
+    }catch (error){
+      console.log(error)
+      seterror({error:true,message:error.message})
+    }
   };
 
+  if(error.error){
+    return <h1>and error occured:{error.message}</h1>
+  }else{
   return(
   <div>
     <h1>Donolad trump quote machine</h1>
@@ -17,6 +29,7 @@ const App = () => {
     <button onClick={handleFetch}>Click</button>
   </div> 
   )
+  }
 }
 
 export default App;
